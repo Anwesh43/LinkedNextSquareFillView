292c0890a0f8912a9f3d5b0f26342f0fbf59305c
Anwesh43 <anweshthecool0@gmail.com>
2020-09-14 02:16:23 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkednextsquarefillview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkednextsquarefillview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkednextsquarefillview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.nextsquarefillview.NextSquareFillView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        NextSquareFillView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
